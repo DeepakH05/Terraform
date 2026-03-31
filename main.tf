347c2ad63e0d5100b2ce2ed7af22635a3b3f9fd3
DeepakH05 <deepak.h0501@gmail.com>
2026-03-31 15:02:31 +0530

    first commit

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ module "eks" {
   source = "./modules/eks"
 
   cluster_name = var.cluster_name
-  cluster_endpoint_public_access_enabled  = true
-  cluster_endpoint_private_access_enabled = true
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnet_ids
 }
