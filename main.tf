b3aaf9eaa6ebc212e40ab15b24e01208c74e6da9
DeepakH05 <deepak.h0501@gmail.com>
2026-03-31 17:56:29 +0530

    first commit

--- main.tf
+++ main.tf
@@ -1,10 +1,17 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  cidr            = var.cidr
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+  azs             = var.azs
+}
+
 module "eks" {
-  source  = "./modules/eks"
+  source = "./modules/eks"
 
   cluster_name = var.cluster_name
   vpc_id       = module.vpc.vpc_id
-
-  # ✅ CHANGE THIS
   subnet_ids   = module.vpc.private_subnet_ids
 
   cluster_endpoint_public_access  = true
